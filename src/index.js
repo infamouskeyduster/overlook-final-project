1da4cb1b713ab817b1d03098eaffc39e84ffb9ca
John Watterson <john@breakthrewbrew.com>
2020-04-17 12:43:44 -0600

    :crystal_ball: :art: Add animation for top bar on manager or customer login

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './images/concierge_desk.jpg';
 import './images/budapest_at_night_2.jpg';
 import './images/manager.jpg';
 import './images/budapest_hotel_only_4.jpg';
+import domUpdates from './dom-updates';
 
 const circleType = new CircleType(document.getElementById('grand-budapest-type'));
 circleType.radius(600);
@@ -25,20 +26,16 @@ $('.login-submit-btn').on('click', () => {
   let password = $('.password-input').val();
   if (username === 'manager' && password === 'overlook2020') {
     console.log('you are logged in as the manager');
-    changeHeaderOnLogin(username);
-    document.body.style.backgroundImage = "url('./images/manager.jpg')";
+    domUpdates.changeHeaderOnLogin(username);
+    domUpdates.changeBodyBackgroundForManager();
   } else if (username.includes('customer') && password === 'overlook2020') {
     console.log('you are logged in as a customer');
-    changeHeaderOnLogin(username);
-    document.body.style.backgroundImage = "url('./images/budapest_hotel_only_4.jpg')";
+    domUpdates.changeHeaderOnLogin(username);
+    domUpdates.changeBodyBackgroundForCustomer();
   } else {
     window.alert('Either your username or password is incorrect. Please try again.');
   }
 });
 
-function changeHeaderOnLogin(username) {
-  $('#welcome-header').text('Welcome:');
-  $('#welcome-header').css('color', '#cc9554');
-  $('#grand-budapest-type').text(username.charAt(0).toUpperCase() + username.slice(1));
-  $('.login-form-box').hide();
-}
+
+//promise all here
