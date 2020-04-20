bacbfa919ea96dc2ac214267f4383c4ed1c6dff0
John Watterson <john@breakthrewbrew.com>
2020-04-19 22:19:41 -0600

    :crystal_ball: :bar_chart: Adds a POST method to apiController that posts to database and calls a domUpdates method; This method still needs testing with spies

--- src/api-controller.js
+++ src/api-controller.js
@@ -1,3 +1,5 @@
+import domUpdates from './dom-updates';
+
 class ApiController {
   constructor() {
   }
@@ -18,6 +20,28 @@ class ApiController {
     let url = 'https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings';
     return fetch(url)
       .then(response => response.json())
+  }
+
+  postBookingForCustomer(id, date, roomNumber) { //<---- spy on this method
+    let bookingPostObj = {
+      'userID': Number(id),
+      'date': date,
+      'roomNumber': Number(roomNumber),
+    };
+      //delete this line below and comment back in the rest when DONE
+      domUpdates.showBookingConfirmationMessage(bookingPostObj);
+    // let url = 'https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings';
+    // return fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(bookingPostObj)
+    // })
+    // .then(response => console.log(response.json()))
+    // .then(domUpdates.showBookingConfirmationMessage(bookingPostObj))
+    // .then(window.alert(`Your booking was successful! We will see you on ${date}!`))
+    // .catch(error => console.log(error.message));
   }
 }
 
