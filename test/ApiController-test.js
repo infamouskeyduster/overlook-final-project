c3f77a65e375fa4b48ac6b660e2fd9e5be58ac6f
John Watterson <john@breakthrewbrew.com>
2020-04-20 21:35:54 -0600

    working to get a spy running in here, ran into issues mocking our a fetch call to get the spy working, thought the spy works when the method is called directly outside of the fetch .then()

---
+++ test/ApiController-test.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import ApiController from '../src/api-controller';
+import domUpdates from '../src/dom-updates';
+const chai = require('chai');
+const spies = require('chai-spies');
+let fetch = {};
+chai.use(spies);
+
+describe('API Controller', function(){
+  let apiController;
+  beforeEach(function(){
+    apiController = new ApiController();
+  });
+
+  it('Should be an instance of ApiController', function(){
+    expect(apiController).to.be.an.instanceof(ApiController);
+  });
+
+  it.skip('Should spy on domUpdates.showBookingConfirmationMessage to verify that it fired after a successful POST request', function(){
+    let bookingPostObj = {
+      'userID': 7,
+      'date': '2020/06/01',
+      'roomNumber': 14,
+    };
+
+    chai.spy.on(domUpdates, 'showBookingConfirmationMessage', () => {});
+    console.log('Fetch needs to be mocked out to spy on this method, but if the fetch call is commeneted out, the SPY is working as expected');
+    apiController.postBookingForCustomer(7, '2020/06/01', 14);
+    expect(domUpdates.showBookingConfirmationMessage).to.have.been.called(1);
+    expect(domUpdates.showBookingConfirmationMessage).to.have.been.called.with(bookingPostObj);
+  });
+});
